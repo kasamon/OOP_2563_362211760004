6cf8958f244639ea4197ea964353e2005437ee71
kasamon <21315@mns.ac.th>
2020-12-29 11:36:04 +0700

    Lab 2 : data types and import data

---
+++ src/LAB2/Exercise_STD_Profile.java
@@ -0,0 +1,43 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class Exercise_STD_Profile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter an name: ");
+        String name = scanner.nextLine();
+
+
+        System.out.println("Enter an id: ");
+        String std_id = scanner.next();
+
+
+        System.out.println("Enter an age: ");
+        int age = scanner.nextInt();
+
+
+        System.out.println("Enter an tel: ");
+        String tel = scanner.next();
+
+
+        System.out.println("Enter an email: ");
+        String email = scanner.next();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+
+}//class
